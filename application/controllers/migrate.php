376c11d07f2d36ef48fc5c29ffa0a42fe2bd2b05
Hendra Gunawan <hendra.gunawan@webimp.com.sg>
2019-01-02 14:20:15 +0700

    adding httaccess file on ci_test folder and create student migration

--- application/controllers/migrate.php
+++ application/controllers/migrate.php
@@ -3,14 +3,17 @@
 class Migrate extends CI_Controller
 {
 
-        public function index()
-        {
-                $this->load->library('migration');
+    public function index()
+    {
+        $this->load->library('migration');
 
-                if ($this->migration->current() === FALSE)
-                {
-                        show_error($this->migration->error_string());
-                }
+        if ( ! $this->migration->current())
+        {
+            echo 'Error ' . $this->migration->error_string();
+            // echo 'ini error';
+        } else {
+            echo 'Migrations ran successfully!';
         }
+    }
 
 }
